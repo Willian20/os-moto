906b6c9ada5c9eddbab9cca4b5d351776995d62f
Willian <willian@willian-Inspiron-1428.(none)>
2013-04-08 19:19:07 -0300

    criação do projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Osmoto::Application.config.secret_token = '397e35da11f2f737942adebd33fdff6ba08dc1bc12b4d65e6b8ef75302e298508a166e257e4df47da92dea8c055a1407764dc9bf4e87e20c70c33fd1c5085cad'
